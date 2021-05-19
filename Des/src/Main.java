214b2a6bef2c9c3f62621895c347c7a72dd74287
HassanKha <58123330+HassanKha@users.noreply.github.com>
2021-05-19 17:20:33 +0200

    MainDes Added

--- Des/src/Main.java
+++ Des/src/Main.java
@@ -2,10 +2,18 @@
 public class Main {
 
 	public static void main(String[] args) {
-		String binaryString="1010";  
-		int decimal=Integer.parseInt(binaryString,2);  
-		System.out.println(decimal); 
- 
+		
+		HandlingInput HI= new HandlingInput("133457799BBCDFF1","0123456789ABCDEF");
+		HI.handleKey();
+		HI.handlePlainText();
+		Step1DES D = new Step1DES(HI.getnewKey());
+		String Kplus = D.GetKeyPlus();
+		D.SplitLeftRight(Kplus);
+		D.GetShiftedKeys();
+		//D.PrintAllKeys();
+		String[] k=D.GetPermutionKeys();
+		Step2DES f= new Step2DES(HI.getnewplaintext(),k);
+        System.out.println(f.step2DES());
 	}
 
 }
--- Des/src/Step2DES.java
+++ Des/src/Step2DES.java
@@ -1,17 +1,33 @@
 import java.util.ArrayList;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
 public class Step2DES {
 	private String plainText;
 	public String[] ShiftedKeys = new String[16];
 
 	private ArrayList<String> L;
 	private ArrayList<String> R;
+	private ArrayList<String> AllM;
+	private ArrayList<String> AllnewM;
 	private int[] IP = { 58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6,
 			64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37,
 			29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7 };
 	private int[] EP = { 32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18,
 			19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1 };
 
+	int[] IP1 = { 40, 8, 48, 16, 56, 24, 64,
+            32, 39, 7, 47, 15, 55,
+            23, 63, 31, 38, 6, 46,
+            14, 54, 22, 62, 30, 37,
+            5, 45, 13, 53, 21, 61,
+            29, 36, 4, 44, 12, 52,
+            20, 60, 28, 35, 3, 43,
+            11, 51, 19, 59, 27, 34,
+            2, 42, 10, 50, 18, 58,
+            26, 33, 1, 41, 9, 49,
+            17, 57, 25 };
+	
 	private int[][][] sbox = {
 			{ { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 },
 					{ 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 },
@@ -55,6 +71,8 @@ public class Step2DES {
 		this.plainText = plainText;
 		this.L = new ArrayList<>();
 		this.R = new ArrayList<>();
+		this.AllM = new ArrayList<>();
+		this.AllnewM = new ArrayList<>();
 		ShiftedKeys = shiftedKeys;
 
 	}
@@ -115,31 +133,92 @@ public class Step2DES {
 		}
 		return PP;
 	}
-
-	public String FRK(String R, String K) {
+	public  int binaryToDecimal(String n)
+    {
+    
+        int dec_value = 0;
+ 
+  
+        int len = n.length();
+        int c=0;
+        for (int i = len - 1; i >= 0; i--) {
+        	char g=n.charAt(i);
+        	
+            if (g == '1') {
+       
+                dec_value += Math.pow(2, c);
+            
+            }
+            c++;
+           
+        }
+   
+ 
+        return dec_value;
+    }
+	public String handlebinary(String g) {
+		
+		int n = 4-g.length();
+		String k="";
+		for(int i=0 ; i<n ; i++) {
+			k+="0";
+		}
+		g=k+g;
+		return g;
+	}
+public String handlehexa(String g) {
+		
+		int n = 2-g.length();
+		String k="";
+		for(int i=0 ; i<n ; i++) {
+			k+="0";
+		}
+		g=k+g;
+		return g;
+	}
+	public String FRK(String R,int in, String K) {
 		String eR = ER(R);
+	
+
 		String eRXK = XOR(eR, K);
+		//System.out.println(eRXK);
 		int counter = 0;
 		String First6 = "";
 		String res = "";
+		int y=0;
 		for (int i = 0; i < eRXK.length(); ++i) {
 			First6 += eRXK.charAt(i);
 
 			if (counter == 5) {
 				String firstPart = "";
 				String secondPart = "";
-				firstPart += First6.charAt(0) + First6.charAt(5);
-				secondPart += First6.charAt(1) + First6.charAt(2) + First6.charAt(3);
-				int fpDecimal = Integer.parseInt(firstPart, 2);
-				int spDeciaml = Integer.parseInt(secondPart, 2);
-				int getsbox = sbox[i][fpDecimal][spDeciaml];
-				res += Integer.toBinaryString(getsbox);
+				
+				firstPart += First6.charAt(0);
+				firstPart += First6.charAt(5);
+			
+				secondPart += First6.charAt(1);  
+				secondPart += First6.charAt(2);
+				secondPart += First6.charAt(3);
+				secondPart += First6.charAt(4);
+		
+				int fpDecimal =binaryToDecimal(firstPart);
+			
+				int spDeciaml = binaryToDecimal(secondPart);
+				
+				int getsbox = sbox[y][fpDecimal][spDeciaml];
+				y++;
+				
+				String g= Integer.toBinaryString(getsbox);
+				g=handlebinary(g);
+				res+=g;
+				
 				First6 = "";
 				counter = 0;
 			} else {
 				counter++;
 			}
 		}
+		
 
 		res = Permetation(res);
 
@@ -147,19 +226,67 @@ public class Step2DES {
 
 	}
 
-	public void step2DES() {
+	public String step2DES() {
 		String IPP = constructIP();
+		
 		String LIPP = GetLeft(IPP);
+	
 		String RIPP = GetRight(IPP);
+	
 		this.L.add(LIPP);
 		this.R.add(RIPP);
 		for (int i = 1; i < 17; ++i) {
 			String NL = this.R.get(i - 1);
 			this.L.add(NL);
-			String NR = XOR(this.L.get(i - 1), FRK(this.R.get(i - 1), this.ShiftedKeys[i - 1]));
+			String NR = XOR(this.L.get(i - 1), FRK(this.R.get(i - 1),i, this.ShiftedKeys[i - 1]));
 			this.R.add(NR);
 		}
+		GetAllPlainTexts();
+		
+		return GetEncrpMessage();
 
+	}
+	
+	public void GetAllPlainTexts() {
+		for(int i=0 ; i<L.size() ; i++) {
+			AllM.add(R.get(i)+L.get(i));
+		}
+		for(int i=0 ; i<AllM.size() ; i++) {
+			String h="";
+			for(int j=0 ; j<IP1.length ; j++) {
+				h+=AllM.get(i).charAt(IP1[j]-1);
+			}
+			AllnewM.add(h);
+		}
+		
+	}
+	public String GetEncrpMessage() {
+		String M=AllnewM.get(AllnewM.size()-1);
+
+		int c1=0;
+		String S1="";
+		
+		
+		int c=0;
+		String S="";
+		String res="";
+		for(int i=0 ; i<M.length() ; i++) {
+			S+=M.charAt(i);
+			
+			if(c==7) {
+				int decimal = Integer.parseInt(S,2);
+				String hexStr = Integer.toString(decimal,16);
+				
+				hexStr=	handlehexa(hexStr);
+				res+=hexStr;
+				S="";
+				c=0;
+			}
+			else {
+				c++;
+			}
+		}
+		return res;
 	}
 
 	public ArrayList<String> getL() {
