51f348a9a73473966b1a6cdca28aa661b807c60e
HassanKha <58123330+HassanKha@users.noreply.github.com>
2021-05-21 00:46:03 +0200

    Add files via upload

--- Des/src/Main.java
+++ Des/src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		HandlingInput HI= new HandlingInput("133457799BBCDFF1","0123456789ABCDEF");
+		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+	    System.out.println("Enter PlainText : ");
+	    String Text = myObj.nextLine(); 
+	    System.out.println("Enter Key : ");
+	    String Key = myObj.nextLine(); 
+		HandlingInput HI= new HandlingInput(Key,Text);
 		HI.handleKey();
 		HI.handlePlainText();
 		Step1DES D = new Step1DES(HI.getnewKey());
@@ -13,7 +18,10 @@ public class Main {
 		//D.PrintAllKeys();
 		String[] k=D.GetPermutionKeys();
 		Step2DES f= new Step2DES(HI.getnewplaintext(),k);
+		System.out.println("Encryption of PlainText is : ");
         System.out.println(f.step2DES());
 	}
 
 }
+//0123456789ABCDEF
+//133457799BBCDFF1
